7fe48108a36f7b8e26572dc0e0a74a01409d039c
LaTlez <95763190+LaTlez@users.noreply.github.com>
2024-04-25 16:23:09 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -75,7 +75,7 @@ true_labels = []
 st.title(f'Image Classifier - {", ".join(classes)}')
 
 # Allow user to upload an image
-uploaded_file = st.file_uploader("Choose an image...", type="jpg|jpeg|png")
+uploaded_file = st.file_uploader("Choose an image...", type="jpeg")
 
 if uploaded_file is not None:
   # Load and preprocess the uploaded image
